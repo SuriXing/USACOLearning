3a4542541ff8198c49f90b8ba60e43e6e7144b6a
Suri Xing <surixing@outlook.com>
2025-06-02 19:03:36 +0800

    I wrote two functions today & the first one is a function that adds/minus two integers (that are entered by the user - the second function determines if a year is a leap year or not

--- chapter3.c
+++ chapter3.c
@@ -1,6 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <stdbool.h>
+
+bool determineLeapYear(int year)
+{
+    if (((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0))
+    {
+        return true; // It's a leap year
+    }
+    else
+    {
+        return false; // It's not a leap year
+    }
+}
+
+int selfcheck_PlusOrMinus()
+{
+    int a, b, c;
+    printf("Enter two integers that are separated by a comma & enter 0 if you want to find their sum and 1 if you want to find their difference: \n");
+    scanf("%d, %d, %d", &a, &b, &c);
+    if (c == 0)
+    {
+        printf("The sum of %d and %d is: %d\n", a, b, a + b);
+    }
+    else if (c == 1)
+    {
+        printf("The difference of %d and %d is: %d\n", a, b, a - b);
+    }
+    else
+    {
+        printf("Invalid input for operation. Please enter 0 for sum or 1 for difference.\n");
+    }
+    return 0;
+}
 
 int sumOf2int()
 {
@@ -28,12 +61,24 @@ int sumOf2int_stepversion()
 int main()
 {
     /*
-    int result = sumOf2int();
+    int resultForSum = sumOf2int();
     printf("The sum of the two integers is: %d\n", result);
-    */
 
     int stepResult = sumOf2int_stepversion();
     printf("The sum of the two integers in step version is: %d\n", stepResult);
+
+    int selfcheckResult = selfcheck_PlusOrMinus();
+    */
+
+    bool result = determineLeapYear(2020);
+    if (result == 1)
+    {
+        printf("2020 is a leap year.\n");
+    }
+    else
+    {
+        printf("2020 is not a leap year.\n");
+    }
 
     return 0;
 }

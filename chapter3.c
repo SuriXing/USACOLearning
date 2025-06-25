d34824f35e8ab629b7d59227e4a5c95a94183d3a
Suri Xing <surixing@outlook.com>
2025-06-25 22:25:14 +0800

    Add simpleSort function to chapter3.c
    Implement a basic sorting algorithm that sorts an integer array in ascending order using nested loops. The function compares adjacent elements and swaps them if they are in the wrong order. This provides a fundamental sorting capability for the chapter3 program.

--- chapter3.c
+++ chapter3.c
@@ -3,6 +3,22 @@
 #include <string.h>
 #include <stdbool.h>
 
+void simpleSort(int array[], int size)
+{
+    for (int i = 0; i < size - 1; i++)
+    {
+        for (int j = i + 1; j < size; j++)
+        {
+            if (array[i] > array[j])
+            {
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+    }
+}
+
 int returnGreaterNum(int a, int b)
 {
     if (a > b)

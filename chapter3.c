57fa60b6bf8239489f631ef360ae0645ce901bcf
Suri Xing <surixing@outlook.com>
2025-07-08 08:09:33 -0700

    Today, I wrote a easier version of pascal's triangle - It's easier aftrer all the different functions about triangle I learnt.

--- chapter3.c
+++ chapter3.c
@@ -49,6 +49,20 @@ void printPascalTriangle(int rows)
     }
 }
 
+void printPascalTriangle_easierversion(int rows)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        int number = 1;
+        for (int j = 0; j <= i; j++)
+        {
+            printf("%d ", number);
+            number = (number * (i - j)) / (j + 1);
+        }
+        printf("\n");
+    }
+}
+
 void printDiagnolTriangle(int rows)
 {
     int current = 1;

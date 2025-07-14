401f6469459cdd7f42e1a5f5ec03111f65dab825
Suri Xing <surixing@outlook.com>
2025-07-13 22:06:17 -0700

    Today I just watched Blackpink's concert in LA and came back to where I lived :) I wrote bubblesort today.

--- chapter3.c
+++ chapter3.c
@@ -3,6 +3,21 @@
 #include <string.h>
 #include <stdbool.h>
 
+void bubbleSort(int arr[], int n)
+{
+    for (int i = 0; i < n - 1; i++)
+    {
+        for (int j = 0; j < n - i - 1; j++)
+        {
+            if (arr[j] > arr[j + 1])
+            {
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+}
 
 int findSmallestNum(int* array, int start, int end)
 {
@@ -364,22 +379,6 @@ void insertionSort(int arr[], int n)
             j--;
         }
         arr[j + 1] = key;
-    }
-}
-
-void bubbleSort(int arr[], int n)
-{
-    for (int i = 0; i < n - 1; i++)
-    {
-        for (int j = 0; j < n - i - 1; j++)
-        {
-            if (arr[j] > arr[j + 1])
-            {
-                int temp = arr[j];
-                arr[j] = arr[j + 1];
-                arr[j + 1] = temp;
-            }
-        }
     }
 }
 

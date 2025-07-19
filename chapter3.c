6225d5a2a1a31fde584c557c2eca8892860b7e24
Suri Xing <surixing@outlook.com>
2025-07-19 08:14:47 -0700

    I rushed to write this today because we have to leave our hotel very soon.

--- chapter3.c
+++ chapter3.c
@@ -3,6 +3,35 @@
 #include <string.h>
 #include <stdbool.h>
 
+void printScalableStar2(int size)
+{
+    for (int i = 0; i < size; i++) printf(" ");
+    printf("*\n");
+    
+    for (int i = 1; i < size; i++)
+    {
+        for (int j = 0; j < size - i; j++) printf(" ");
+        printf("*");
+        for (int j = 0; j < 2*i - 1; j++) printf(" ");
+        printf("*\n");
+    }
+    
+    printf(" ");
+    for (int i = 0; i < 2*size - 1; i++) printf("*");
+    printf("\n");
+    
+    for (int i = size - 1; i > 0; i--)
+    {
+        for (int j = 0; j < size - i; j++) printf(" ");
+        printf("*");
+        for (int j = 0; j < 2*i - 1; j++) printf(" ");
+        printf("*\n");
+    }
+    
+    for (int i = 0; i < size; i++) printf(" ");
+    printf("*\n");
+}
+
 int fourleafCloverNum(int num)
 {
     int ones = 0;

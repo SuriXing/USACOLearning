eb01d18dee93557b15844fe57cbfae142aaf9d0d
Suri Xing <surixing@outlook.com>
2025-08-06 23:22:53 +0800

    I did another USACO bronze level problem today TD this problem is very similar to a practice I did a few weeks ago

--- chapter3.c
+++ chapter3.c
@@ -441,6 +441,25 @@ void printLeftToRightTriangle(int rows)
     }
 }
 
+void printLeftToRightTriangle2(int rows)
+{
+    int num = 1;
+    for (int i = 1; i <= rows; i++)
+    {
+        int leadingSpaces = (rows - i) * 3;
+        for (int space = 0; space < leadingSpaces; space++)
+        {
+            printf(" ");
+        }
+        
+        for (int j = 1; j <= i; j++)
+        {
+            printf("%2d ", num++);
+        }
+        printf("\n");
+    }
+}
+
 int sumOfFraction(int n)
 {
     int count = 0;

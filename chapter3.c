4abf38c814865f6e8da6d19f9bfd66e834ed9a77
Suri Xing <surixing@outlook.com>
2025-09-11 22:13:05 +0800

    Today, I let Ai rate my codes all along and fixed the problems. I leanrt that I should add preventions to every functions - (prevention = prevent numbers from overflow).

--- chapter3.c
+++ chapter3.c
@@ -6,7 +6,7 @@
 #include <limits.h>
 #include <assert.h>
 
-int selfcheck_PlusOrMinus()
+int checkOperation()
 {
     int a, b, c;
     printf("Enter two integers that are separated by a comma & enter 0 if you want to find their sum and 1 if you want to find their difference: \n");
@@ -26,7 +26,7 @@ int selfcheck_PlusOrMinus()
     return 0;
 }
 
-int sumOf2int()
+int sumOfTwoIntegers()
 {
     int a, b;
     printf("Enter two integers in the form of integer a, integer b: \n");
@@ -35,7 +35,7 @@ int sumOf2int()
     return a+b;
 }
 
-int sumOf2int_stepversion()
+int sumOfTwoIntegersWithSteps()
 {
     int a, b;
     printf("Enter two integers in the form of integer a, integer b: \n");
@@ -51,88 +51,8 @@ int sumOf2int_stepversion()
 
 int main()
 {
-
-    int array[5] = {12, 33, 12, 41, 1};
-
-    int grades[10] = {37, 90, 100, 90, 40 , 25, 18, 59, 1, 10};
-
-    /*
-    int resultForSum = sumOf2int();
-    printf("The sum of the two integers is: %d\n", result);
-
-    int stepResult = sumOf2int_stepversion();
-    printf("The sum of the two integers in step version is: %d\n", stepResult);
-
-    int selfcheckResult = selfcheck_PlusOrMinus();
-
-    bool result = determineLeapYear(2020);
-    if (result == 1)
-    {
-        printf("2020 is a leap year.\n");
-    }
-    else
-    {
-        printf("2020 is not a leap year.\n");
-    }
-
-    int triangleResult = determineTriangle(3, 4, 12);
-    printf("The triangle side length is %s\n", triangleResult ? "valid" : "not valid");
-
-    twoIntegersCalculation();
-    
-    returnCommentToGrades();
-
-    printAstrics(7);
-
-    printArray(array, 5);
-
-    makeArraySmallToBig(array, 5);
-
-    printArray(array, 5);
-
-    abcd();
-
-    int sum = evenNumSum(4);
-    printf("%d\n", sum);
-
-    int gcd = gcdOfTwoNum(17, 25);
-    printf("The gcd of 17 & 25 is %d\n", gcd);
-
-    int count = howManyLowerThanAverage(grades);
-    printf("%d\n", count);
-    
-    returnSquare(5);
-
-    int lcm = findLcmOfTwoInt(12, 18);
-    printf("%d\n", lcm);
-
-    int sumOfSquare = sumOfSquares_seven(21);
-    printf("Result: %d\n", sumOfSquare);
-    
-    int arr[] = {5, 2, 9, 1, 5, 6};
-    bubbleSort(arr, 6);
-    
-    int numberInStr = countNotSpaceChars("ABCD123 674");//10
-    printf("%d\n", numberInStr);
-    
-    printLeftToRightTriangle(6);
-    
-    printMultiplicationTable_81();
-
-    printCenteredPascalTriangle(7);
-
-    printRectangleOfAstrics(3,5);
-
-    printScalableStar(5);
-
-    fourleafCloverNum(1634);
-
-    findTreasure(5, 6);
-
-    fenceProblem();
-    
-    printLeftToRightTriangle(6);
+    checkOperation();
+    sumOfTwoIntegers();
+    sumOfTwoIntegersWithSteps();
     return 0;
-    */
-    nlanterns(6);
 }

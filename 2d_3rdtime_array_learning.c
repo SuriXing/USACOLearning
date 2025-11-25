0ff2f1efd6c7da3caa294b205efb742fb4c2db2c
Suri Xing <surixing@outlook.com>
2025-11-25 21:32:31 +1000

    Today I implemented findMax and findMin, I can easily tell the boundary and write bug free code

--- 2d_3rdtime_array_learning.c
+++ 2d_3rdtime_array_learning.c
@@ -15,6 +15,12 @@ bool array2DIsFull(Array2D* pArray);
 
 bool array2DIsEmpty(Array2D* pArray);
 
+bool array2DPrint(Array2D* pArray);
+
+int array2DFindMax(Array2D* pArray);
+
+int array2DFindMin(Array2D* pArray);
+
 bool array2DIsFull(Array2D* pArray)
 {
     assert(NULL != pArray);
@@ -29,6 +35,82 @@ bool array2DIsEmpty(Array2D* pArray)
     return (pArray->numOfItems == 0);
 }
 
+
+bool array2DPrint(Array2D* pArray)
+{
+    assert(NULL != pArray);
+
+    for (int i = 0; i < ROW_LENGTH; i++)
+    {
+        for (int j = 0; j < COL_LENGTH; j++)
+        {
+            printf("%d\t", pArray->items[i][j]);
+        }
+        printf("\n");
+    }
+
+    printf("\n");
+
+    return true;
+}
+
+
+int array2DFindMax(Array2D* pArray)
+{
+    assert(NULL != pArray);
+
+    int max = pArray->items[0][0];
+
+    for (int i = 0; i < ROW_LENGTH; i++)
+    {
+        for (int j = 0; j < COL_LENGTH; j++)
+        {
+            if (max < pArray->items[i][j])
+            {
+                max = pArray->items[i][j];
+            }
+        }
+    }
+
+    for (int i = 0; i < pArray->numOfItems % COL_LENGTH; i++)
+    {
+        if (max < pArray->items[pArray->numOfItems/COL_LENGTH][i])
+        {
+            max = pArray->items[pArray->numOfItems/COL_LENGTH][i];
+        }
+    }
+
+    return max;
+}
+
+int array2DFindMin(Array2D* pArray)
+{
+    assert(NULL != pArray);
+
+    int min = pArray->items[0][0];
+
+    for (int i = 0; i < ROW_LENGTH; i++)
+    {
+        for (int j = 0; j < COL_LENGTH; j++)
+        {
+            if (min > pArray->items[i][j])
+            {
+                min = pArray->items[i][j];
+            }
+        }
+    }
+
+    for (int i = 0; i < pArray->numOfItems%COL_LENGTH; i++)
+    {
+        if (min > pArray->items[pArray->numOfItems/COL_LENGTH][i])
+        {
+            min = pArray->items[pArray->numOfItems/COL_LENGTH][i];
+        }
+    }
+
+    return min;
+}
+
 int main()
 {
     Array2D testArray =
@@ -40,6 +122,10 @@ int main()
         10
     };
 
+    array2DPrint(&testArray);
+
+    printf("array find max: %d\n", array2DFindMax(&testArray));
+    printf("array find min: %d\n", array2DFindMin(&testArray));
 
     return 0;
 }

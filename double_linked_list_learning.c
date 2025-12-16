e0a62115a852220108282d178db0da5e58186a1a
Suri Xing <surixing@outlook.com>
2025-12-16 22:13:45 +0800

    Today I implemented AddItem, it is different from single linked list, I need update prev of next node and next of previous node

--- double_linked_list_learning.c
+++ double_linked_list_learning.c
@@ -22,6 +22,8 @@ bool doubleLinkedListIsEmpty(DoubleLinkedList* pList);
 int doubleLinkedListFindMax(DoubleLinkedList* pList);
 int doubleLinkedListFindMin(DoubleLinkedList* pList);
 
+bool doubleLinkedListAddItem(DoubleLinkedList* pList, int newItem);
+
 bool doubleLinkedListIsEmpty(DoubleLinkedList* pList)
 {
     assert(NULL != pList);
@@ -103,12 +105,54 @@ int doubleLinkedListFindMin(DoubleLinkedList* pList)
     return minValue;
 }
 
+bool doubleLinkedListAddItem(DoubleLinkedList* pList, int newItem)
+{
+    assert(NULL != pList);
+
+    Node* pNewNode = (Node*)malloc(sizeof(Node));
+    if (NULL == pNewNode)
+    {
+        return false;
+    }
+    pNewNode->item = newItem;
+    pNewNode->pNext = NULL; 
+    pNewNode->pPrev = NULL; 
+
+    Node* pCurrent = &pList->dummyHead;
+    while (NULL != pCurrent->pNext)
+    {
+        pCurrent = pCurrent->pNext;
+    }
+
+    pCurrent->pNext = pNewNode;
+    pNewNode->pPrev = pCurrent;
+    
+    return true;
+}
+
 int main()
 {
     DoubleLinkedList list;
     list.dummyHead.pPrev = NULL; 
     list.dummyHead.pNext = NULL;
 
+    doubleLinkedListAddItem(&list, 6);
+    doubleLinkedListAddItem(&list, 6);
+    doubleLinkedListAddItem(&list, 8);
+    doubleLinkedListAddItem(&list, 8);
+    doubleLinkedListAddItem(&list, 8);
+    doubleLinkedListAddItem(&list, 1);
+    
+    //print
+    doubleLinkedListPrint(&list);
+
+    //max
+    printf("Double Linked List find Max:\n");
+    printf("Max value: %d\n", doubleLinkedListFindMax(&list));
+
+    //min
+    printf("Double Linked List find Min:\n");
+    printf("Min value: %d\n", doubleLinkedListFindMin(&list));
 
     return 0;
 }
